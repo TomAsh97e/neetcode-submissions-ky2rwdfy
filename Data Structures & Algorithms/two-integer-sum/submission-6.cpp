c2cd5cbb77f7b00cc60bed9221bc3ffdce7dd842
TomAsh <tomek.zycki97@gmail.com>
2026-04-10 09:52:35 +0200

    Add: two-integer-sum - submission-6

---
+++ Data Structures & Algorithms/two-integer-sum/submission-6.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map <int,int> indeksy;
+        for(int i = 0; i < nums.size();i++){
+            indeksy[nums[i]] = i;
+        }
+
+        for (int i=0; i < nums.size(); i++){
+            int diff = target - nums[i];
+            if(indeksy.count(diff) ==1 && indeksy[diff] != i)
+            return{i,indeksy[diff]};
+        }
+        return{};
+    }
+};
